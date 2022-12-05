276fb6fe8df1a969c24382ec6b6af6e444d12a14
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2022-12-05 03:37:07 +0100

    change database from local to remote

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // mongodb://localhost:27017/expmapDB
 // mongodb+srv://ewurufortune:pmX0GdJHQumI8jVb@resourcecluster.inplmwx.mongodb.net/roadmapDB
-mongoose.connect("mongodb://localhost:27017/expmapDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ewurufortune:pmX0GdJHQumI8jVb@resourcecluster.inplmwx.mongodb.net/roadmapDB", {useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
 const roadmapSchema = new mongoose.Schema ({
